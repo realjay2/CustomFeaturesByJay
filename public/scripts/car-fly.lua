622082f67005a87a1c93fb9e23fc261bb829408b
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 16:53:28 -0800

    Update car-fly.lua

--- public/scripts/car-fly.lua
+++ public/scripts/car-fly.lua
@@ -1,3 +1,11 @@
+repeat task.wait() until _G.WindUI and _G.Functions
+
+local WindUI       = _G.WindUI
+local Window       = _G.Window
+local Tabs         = _G.Tabs
+local Functions    = _G.Functions
+local Connections  = _G.Connections
+
 getgenv()._CarFlySpeed = 50 
 
 Tabs.Custom:Toggle({

91232175981ef13758abec99bc303a4e5cc04b0d
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 16:23:02 -0800

    Create check-erx.lua

---
+++ public/scripts/check-erx.lua
@@ -0,0 +1,11 @@
+if not _G.RanERX then
+    local StarterGui = game:GetService("StarterGui")
+
+    StarterGui:SetCore("SendNotification", {
+        Title = "ERX (CUSTOM FEATURES)";
+        Text = "ERX has not loaded yet. Please start ERX.";
+        Duration = 5;
+    })
+
+    return
+end

29fb8a42b8191c2d8cca7985ba80a79f58a733bd
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 16:27:57 -0800

    Fix indentation and return statement in key.lua

--- public/scripts/key.lua
+++ public/scripts/key.lua
@@ -3,8 +3,7 @@ local required_key = "YglKravMHm"
 if typeof(script_key) ~= "string" or script_key ~= required_key then
     if game and game.Players then
         game.Players.LocalPlayer:Kick("Missing or invalid Script Key.")
-    end
-    return 
+    end 
 end
 
 print("UserID Encrypted")

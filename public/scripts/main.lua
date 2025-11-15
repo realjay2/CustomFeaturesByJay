29217d1acc204962807a0c4be869436a666dfdd4
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 16:23:12 -0800

    Update main.lua

--- public/scripts/main.lua
+++ public/scripts/main.lua
@@ -2,6 +2,7 @@ local version = "3.12"
 
 loadstring(game:HttpGet("https://luamour.vercel.app/scripts/key.lua"))()
 loadstring(game:HttpGet("https://luamour.vercel.app/scripts/executors.lua"))()
+loadstring(game:HttpGet("https://luamour.vercel.app/scripts/check-erx.lua"))()
 
 print("Loaded | Custom Features")
 print("Version: " .. version)

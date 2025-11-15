6d5c0d1194cd85ddc2e9103bc7788439c7cfdd6e
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 17:35:03 -0800

    Create Kill-All.lua

---
+++ public/scripts/Kill-All.lua
@@ -0,0 +1,20 @@
+repeat task.wait() until _G.WindUI and _G.Functions
+
+local WindUI = _G.WindUI
+local Tabs = _G.Tabs
+local Functions = _G.Functions
+
+-------------------------------------------------
+Tabs.Custom:Section({
+	Title = "Kill All",
+	TextSize = 16,
+})
+
+Tabs.Custom:Toggle({
+    Title = "Kill All",
+    Desc = "I have the method, but Dorblx said I cant release :(",
+    Default = false,
+    Locked = True
+    Callback = function(value)
+    end
+})

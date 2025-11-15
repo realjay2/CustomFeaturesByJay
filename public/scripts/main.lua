92b6e7c1de6598411e6439bb8de51cb48cb2d3e3
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 17:29:40 -0800

    Update main.lua

--- public/scripts/main.lua
+++ public/scripts/main.lua
@@ -10,13 +10,16 @@ loadstring(game:HttpGet("https://luamour.vercel.app/scripts/check-erx.lua"))()
 loadstring(game:HttpGet("https://luamour.vercel.app/scripts/key.lua"))()
 loadstring(game:HttpGet("https://luamour.vercel.app/scripts/executors.lua"))()
 
--- Wait until WindUI, Window, Tabs, and Functions exist
 repeat task.wait() until _G.WindUI and _G.Tabs and _G.Functions and _G.Window
 
 local WindUI = _G.WindUI
 local Tabs = _G.Tabs
 local Functions = _G.Functions
 local Window = _G.Window
+
+-- Load Rejoin Script
+
+loadstring(game:HttpGet("https://luamour.vercel.app/scripts/Rejoin.lua"))()
 
 Tabs.Custom = Window:Tab({
     Title = "Custom Features",

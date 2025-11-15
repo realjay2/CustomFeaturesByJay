fbd63fe480238ca35db84344bb2869e39e346c5b
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 17:57:57 -0800

    Update Auto-Arrest.lua

--- public/scripts/Auto-Arrest.lua
+++ public/scripts/Auto-Arrest.lua
@@ -14,7 +14,7 @@ Tabs.Custom:Toggle({
     Title = "Auto Arrest",
     Desc = "Auto Arrest V2 (Soon)",
     Default = false,
-    Locked = True,
+    Locked = true,
     Callback = function(value)
     end
 })

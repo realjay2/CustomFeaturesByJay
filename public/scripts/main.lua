256a520de3c782cf6ba077c9c9ebcb788e184fa3
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 16:17:39 -0800

    Create main.lua

---
+++ public/scripts/main.lua
@@ -0,0 +1,7 @@
+local version = "3.12"
+
+loadstring(game:HttpGet("https://luamour.vercel.app/scripts/key.lua"))()
+loadstring(game:HttpGet("https://luamour.vercel.app/scripts/executors.lua"))()
+
+print("Loaded | Custom Features")
+print("Version: " .. version)

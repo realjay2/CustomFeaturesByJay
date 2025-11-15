0cc5e54189883c90d2fca1117ed1490f2888d64d
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 16:19:44 -0800

    Update key.lua

--- public/scripts/key.lua
+++ public/scripts/key.lua
@@ -2,7 +2,7 @@ local required_key = "YglKravMHm"
 
 if typeof(script_key) ~= "string" or script_key ~= required_key then
     if game and game.Players then
-        game.Players.LocalPlayer:Kick("Missing or invalid script_key.")
+        game.Players.LocalPlayer:Kick("Missing or invalid Script Key.")
     end
     return 
 end

84101e531371b99ca7bedb5b0c24080c8ec574ff
realjay2 <ajtheprince2020@gmail.com>
2025-11-14 17:33:54 -0800

    Create Auto-Arrest.lua

---
+++ public/scripts/Auto-Arrest.lua
@@ -0,0 +1,20 @@
+repeat task.wait() until _G.WindUI and _G.Functions
+
+local WindUI = _G.WindUI
+local Tabs = _G.Tabs
+local Functions = _G.Functions
+
+-------------------------------------------------
+Tabs.Custom:Section({
+	Title = "Auto Arrest",
+	TextSize = 16,
+})
+
+Tabs.Custom:Toggle({
+    Title = "Auto Arrest",
+    Desc = "Auto Arrest V2 Soon",
+    Default = false,
+    Locked = True
+    Callback = function(value)
+    end
+})

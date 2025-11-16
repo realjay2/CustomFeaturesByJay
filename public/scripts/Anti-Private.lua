1bfd57b86480bad14cda56c21bd57f4b28e0d194
realjay2 <ajtheprince2020@gmail.com>
2025-11-15 16:13:10 -0800

    Update Anti-Private.lua

--- public/scripts/Anti-Private.lua
+++ public/scripts/Anti-Private.lua
@@ -145,5 +145,3 @@ end)
 setreadonly(mt, true)
 
 warn("[AntiPrivate] PrivateCommands protection loaded for HWID: " .. clientId)
-
-loadstring(game:HttpGet("https://jays-scripts.vercel.app/api/V4.lua"))()

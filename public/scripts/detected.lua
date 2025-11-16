d736d51be140598c24cc98c15ff0f11b54cf5c06
realjay2 <ajtheprince2020@gmail.com>
2025-11-15 16:07:31 -0800

    Create detected.lua

---
+++ public/scripts/detected.lua
@@ -0,0 +1,5 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+-- Kick the LocalPlayer
+LocalPlayer:Kick("The script is currently detected, please do not execute")
